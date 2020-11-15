82c89efc0a857f4a12246b934ce20c6fc93994b1
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-11-15 16:29:05 +0100

    First red test : a_panini_collector_should_see_his_empty_collection

--- src/test/java/fr/tarcaye/panini/CollectionTest.java
+++ src/test/java/fr/tarcaye/panini/CollectionTest.java
@@ -2,12 +2,12 @@ package fr.tarcaye.panini;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CollectionTest {
 
     @Test
     void a_panini_collector_should_see_his_empty_collection() {
-
+        assertThat("").isEqualTo("Votre collection est vide.");
     }
 }
