7715fb1f7104bbad86a1c55a81bee3535b75ecab
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-11-15 16:24:31 +0100

    Add a first test

---
+++ src/main/java/fr/tarcaye/panini/Collection.java
@@ -0,0 +1,4 @@
+package fr.tarcaye.panini;
+
+public class Collection {
+}
---
+++ src/test/java/fr/tarcaye/panini/CollectionTest.java
@@ -0,0 +1,13 @@
+package fr.tarcaye.panini;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionTest {
+
+    @Test
+    void a_panini_collector_should_see_his_empty_collection() {
+
+    }
+}
